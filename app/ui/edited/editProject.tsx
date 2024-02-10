bbdbd3bfb6b87af0c1597514aa144ca740b2afbf
Roddy <adarkwagodred7075@icloud.com>
2024-02-10 21:17:52 +0000

    editProject forms added

--- app/ui/edited/editProject.tsx
+++ app/ui/edited/editProject.tsx
@@ -5,6 +5,7 @@ import { motion, useAnimation } from "framer-motion";
 import { useEffect } from "react";
 import { slideFromTop } from "./animation";
 import Cancel from "../buttons/cancel";
+import EditForm from "./editForm";
 
 export default function EditProject() {
 
@@ -21,12 +22,15 @@ export default function EditProject() {
          variants={slideFromTop}
          animate={control}
          initial='hide'
-         className="md:w-[55%]  w-[95%] md:h-[80%] h-[90%] xl:h-[65%] rounded-[45px] bg-white p-6">
+         className="md:w-[55%]  w-[95%] xl:w-[45%] md:h-[80%] h-[90%] xl:h-[68%] rounded-[45px] bg-white p-6">
             <div className="flex justify-between ">
               <h2 className="font-[600] text-light_blue">Edit Project</h2>
               <div className="cursor-pointer">
                   <Cancel/>
               </div>
+            </div>
+            <div className="flex flex-col my-4">
+              <EditForm/>
             </div>
           </motion.div>
     </>
